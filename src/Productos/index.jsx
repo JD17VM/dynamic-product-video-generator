b94d39dfee13680e0d75ea9dac8e63a25b85ab2e
JD17VM <jdiegodiego4@gmail.com>
2024-11-13 22:37:51 -0500

    :sparkles: feat(video): add Video en base a formato JSON

--- src/Productos/index.jsx
+++ src/Productos/index.jsx
@@ -1,14 +1,21 @@
-import { AbsoluteFill, useVideoConfig, useCurrentFrame } from 'remotion';
+import { AbsoluteFill, useVideoConfig, useCurrentFrame, Sequence } from 'remotion';
 import { Producto_Unico } from './Producto_Unico';
 import { Producto_Doble } from './Producto_Doble';
 
 import styles from '../assets/styles/estilos_producto.module.css'
 
 import { Pieza_Izquierda, Pieza_Derecha } from "./Piezas_Fondo";
-import imageHelper from '../utils/imageHelper';
 
-export const Productos = () => {
+export const Productos = ({ data = [] }) => {
     const { fps } = useVideoConfig();
+
+    // Duración por producto
+    const entradaDuracion = 10;
+    const duracionVisible = 10 * fps;
+    const salidaDuracion = 10;
+    const descanso = 3 * fps;
+    const duracionPorProducto = entradaDuracion + duracionVisible + salidaDuracion + descanso;
+
     return (
         <AbsoluteFill style={{
           backgroundColor: "white"
@@ -19,28 +26,41 @@ export const Productos = () => {
           <Pieza_Derecha />
         </div>
 
-
-        {/*
-        <Producto_Unico
-          enlace_imagen={imageHelper.ImagenProducto4}
-          texto={"Este es el texto de un producto"}
-          precio={"S/. 1550.00"}
-          relativeFrame={useCurrentFrame() - 0} // Calcular el frame relativo
-          fps={fps}
-        />
-        */}
-
-        <Producto_Doble
-          enlace_imagen_1={imageHelper.ImagenProducto1}
-          enlace_imagen_2={imageHelper.ImagenProducto3}
-          texto_1={"Este es el texto de un producto numero uno"}
-          texto_2={"Este es el texto de un producto numero dos"}
-          precio_1={"S/. 1550.00"}
-          precio_2={"S/. 2100.00"}
-          relativeFrame={useCurrentFrame() - 0} // Calcular el frame relativo
-          fps={fps}
-        />
-
+        {data.map((producto, index) => {
+          const offset = index * duracionPorProducto; // Calcular el offset para este producto
+          return (
+            <Sequence
+              key={index}
+              from={offset}
+              durationInFrames={duracionPorProducto}
+            >
+            
+              {!producto.producto_doble ? (
+              <Producto_Unico
+                enlace_imagen={producto.enlace_imagen}
+                texto={producto.texto}
+                precio={producto.precio}
+                relativeFrame={useCurrentFrame() - offset} // Calcular el frame relativo
+                fps={fps}
+                index={index}
+              />
+              ) : (
+              <Producto_Doble
+                enlace_imagen_1={producto.enlace_imagen_1}
+                enlace_imagen_2={producto.enlace_imagen_2}
+                texto_1={producto.texto_1}
+                texto_2={producto.texto_2}
+                precio_1={producto.precio_1}
+                precio_2={producto.precio_2}
+                relativeFrame={useCurrentFrame() - offset} // Calcular el frame relativo
+                fps={fps}
+                index={index}
+              />
+              )}
+              
+            </Sequence>
+          );
+        })}
       </AbsoluteFill>
     );
 };
--- src/Root.jsx
+++ src/Root.jsx
@@ -2,10 +2,65 @@ import { Composition } from "remotion";
 import { Productos } from './Productos';
 import './assets/styles/estilos_generales.css'
 
+import imageHelper from "./utils/imageHelper";
+
+// Datos que quieres pasar al componente Productos
+const productosData = {
+  "data": [
+    {
+      "producto_doble": true,
+      "enlace_imagen_1": imageHelper.ImagenProducto1,
+      "texto_1": "Este es el prdsgoducto 01",
+      "precio_1": "S/. 100.00",
+      "enlace_imagen_2": imageHelper.ImagenProducto2,
+      "texto_2": "Este es el psdfgsfroducto 02",
+      "precio_2": "S/. 1340.sdfg00"
+    },
+    {
+      "producto_doble": false,
+      "enlace_imagen": imageHelper.ImagenProducto3,
+      "texto": "Este es el producto sdfg03",
+      "precio": "S/. 20sdfg0.00"
+    },
+    {
+      "producto_doble": false,
+      "enlace_imagen": imageHelper.ImagenProducto4,
+      "texto": "Este es el prsdfgoducto 04",
+      "precio": "S/. 1500sdg.00"
+    },
+    {
+      "producto_doble": true,
+      "enlace_imagen_1": imageHelper.ImagenProducto1,
+      "texto_1": "Este es el psdgfdsfgroducto 05",
+      "precio_1": "S/. 13sdg50.00",
+      "enlace_imagen_2": imageHelper.ImagenProducto3,
+      "texto_2": "Este es el producto 06",
+      "precio_2": "S/. 1700dd.00"
+    },
+    {
+      "producto_doble": false,
+      "enlace_imagen": imageHelper.ImagenProducto4,
+      "texto": "Este es el producto 07",
+      "precio": "S/. 30sf0.00"
+    },
+    {
+      "producto_doble": false,
+      "enlace_imagen": imageHelper.ImagenProducto1,
+      "texto": "Este es el producto 08",
+      "precio": "S/. 50fsg0.00"
+    }
+  ],
+  "metadata": {
+    "nombre_video": "Video_D_4"
+  }
+};
+
 export const RemotionRoot = () => {
   const fps = 30
   const segundos = 10
-  const duracionTotalVideo = (fps * segundos) + 1 // 1 frame más para que dure el tiempo exacto
+  const duracionProductoEnFrames = (segundos + 3.19) * fps
+  const cantidadTotalProductos = productosData.data.length
+  const duracionTotalVideo = Math.floor((duracionProductoEnFrames * cantidadTotalProductos) + 1) // 1 frame más para que dure el tiempo exacto
 
   return (
     <>
@@ -16,6 +71,7 @@ export const RemotionRoot = () => {
         fps={fps}
         width={1920}
         height={1080}
+        defaultProps={productosData}
       />
     </>
   );
